7309289c5e0f914f323b8e9f770b5f801750a831
Stephen Costeira <coste011@rangers.uwp.edu>
2016-06-01 12:21:26 -0500

    added 'take again' button

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -37,6 +37,9 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        console.log("received event " + id);
+
+        //this just hides some splash screen stuff
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -44,109 +47,99 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
- 
         //hide loading bar
         document.getElementsByClassName('app')[0].setAttribute('style', 'display:none;');
 
-        //display my stuff
-        //document.getElementsByClassName('picture')[0].setAttribute('style', 'display:block;');
+        //bind the 'take again' button
+        document.getElementById('takeAgain').onclick = app.takePicture;
 
+        //take the picture
+        app.takePicture();
+    },
+
+    takePicture: function(){
         //Picture stuff
-        navigator.camera.getPicture(onSuccess, onFail, {
+        navigator.camera.getPicture(app.pictureTaken, app.pictureFailedToTake, {
             quality: 25,
             encodingType: Camera.EncodingType.JPEG,
             destinationType: Camera.DestinationType.FILE_URI,
             targetWidth: window.innerWidth,
             targetHeight: window.innerHeight
         });
+    },
 
-        function onSuccess(imageData) {
-            //show image in picture div
-            var image = document.getElementById('yes');
-            image.src = imageData;
-    
+    pictureTaken: function(imageData){
+        //show image in picture div
+        var image = document.getElementById('yes');
+        image.src = imageData;
 
-            //stuff
-            // set up some sample squares
-            var example = document.getElementById('example');
-            var context = example.getContext('2d');
-            example.height = window.innerHeight - 100;
-            example.width = window.innerWidth;
+        // set up
+        var example = document.getElementById('example');
+        var context = example.getContext('2d');
+        example.height = window.innerHeight - 100;
+        example.width = window.innerWidth;
 
-            //other div
-            var statusDiv = document.getElementById('status');
-            statusDiv.height = 150;
-            statusDiv.width = 300;
+        //other div
+        var statusDiv = document.getElementById('status');
+        statusDiv.height = 150;
+        statusDiv.width = 300;
 
-            var otherContext = statusDiv.getContext('2d');
+        var otherContext = statusDiv.getContext('2d');
 
-            //wait until picture is loaded
-            image.onload = function () {
-                context.drawImage(image, 0, 0);
+        //wait until picture is loaded
+        image.onload = function () {
+            context.drawImage(image, 0, 0);
 
-                otherContext.fillStyle = "rgb(255,0,0)";
-                otherContext.fillRect(0, 0, 50, 50);
-                otherContext.fillStyle = "rgb(0,0,255)";
-                otherContext.fillRect(55, 0, 50, 50);
-            }
-
-
-            //hexcode variable
-            var hex = "#000000";
-
-            //touch stuff
-            example.onclick = (function (e) {
-               
-                var pos = findPos(this);
-                var x = e.pageX - pos.x;
-                var y = e.pageY - pos.y;
-                var c = this.getContext('2d');
-                var p = c.getImageData(x, y, 1, 1).data;
-                hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-                alert(x + " " + y + " " + hex);
-
-                //should draw the boxes
-
-
-                otherContext.fillStyle = hex;
-                otherContext.fillRect(0, 0, 50, 50);
-                otherContext.fillStyle = hex;
-                otherContext.fillRect(55, 0, 50, 50);                
-
-            });
-
-
-
-
-        }
-
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
-        function findPos(obj) {
-            var curleft = 0, curtop = 0;
-            if (obj.offsetParent) {
-                do {
-                    curleft += obj.offsetLeft;
-                    curtop += obj.offsetTop;
-                } while (obj = obj.offsetParent);
-                return { x: curleft, y: curtop };
-            }
-            return undefined;
-        }
-
-        function rgbToHex(r, g, b) {
-            if (r > 255 || g > 255 || b > 255)
-                throw "Invalid color component";
-            return ((r << 16) | (g << 8) | b).toString(16);
+            otherContext.fillStyle = "rgb(255,0,0)";
+            otherContext.fillRect(0, 0, 50, 50);
+            otherContext.fillStyle = "rgb(0,0,255)";
+            otherContext.fillRect(55, 0, 50, 50);
         }
 
 
+        //hexcode variable
+        var hex = "#000000";
 
+        //touch stuff
+        example.onclick = app.pictureClicked;
+    },
+    pictureFailedToTake: function(message){
+        alert('Failed because: ' + message);
+    },
+    pictureClicked: function(e){
+        console.log("canvas clicked");
+        var otherContext = document.getElementById('status').getContext('2d');
 
+        var pos = app.canvasFindPos(this);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+        var c = this.getContext('2d');
+        var p = c.getImageData(x, y, 1, 1).data;
+        hex = "#" + ("000000" + app.rgbToHex(p[0], p[1], p[2])).slice(-6);
+        console.log(x + " " + y + " " + hex);
 
+        //should draw the boxes
+        otherContext.fillStyle = hex;
+        otherContext.fillRect(0, 0, 50, 50);
+        otherContext.fillStyle = hex;
+        otherContext.fillRect(55, 0, 50, 50);                
 
-        
-        console.log('Received Event: ' + id);
+        console.log("bottom of click");
+    },
+    canvasFindPos: function(obj){
+        var curleft = 0, curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curleft += obj.offsetLeft;
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return { x: curleft, y: curtop };
+        }
+        return undefined;
+    },
+    rgbToHex: function(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
     }
 };
